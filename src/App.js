48ce6723f13fed92f12e8147cb33c9ee714df965
theam-dev <phearakroun@gmail.com>
2023-04-02 16:40:46 +0700

    React Js Remove Row

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+const App = () => {
+  const [rows, setRows] = useState([
+    { id: 1, name: "Alice", phone: "1234567890", email: "alice@example.com" },
+    { id: 2, name: "Bob", phone: "0987654321", email: "bob@example.com" },
+    { id: 3, name: "Charlie", phone: "1357924680", email: "charlie@example.com" },
+  ]);
 
-function App() {
+  const deleteRow = (index) => {
+    // make a copy of the rows array
+    const newRows = [...rows];
+    // remove one element at index
+    newRows.splice(index, 1);
+    // update the state with the new array
+    setRows(newRows);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding: "20px"}}>
+      <h4>Tutorial Sharing- React Remove Row</h4>
+      <br/>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row, index) => (
+            <tr key={row.id}>
+              <td>{row.name}</td>
+              <td>{row.phone}</td>
+              <td>{row.email}</td>
+              <td>
+                <button onClick={() => deleteRow(index)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
